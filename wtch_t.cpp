49462f53c74c0df7fe4adfd90c6b2d6a9fb40c5b
himanshu06d <hd156681@gmail.com>
2020-02-29 19:52:36 +0530

    new thing

---
+++ wtch_t.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  char c[]={"HIMANSHU DUBEY"};// character type array string
+  cout<<c<<endl;
+
+  wchar_t wa[]={L"HEY!!"};// wide char type array string
+  wcout<<wa<<endl;
+return 0;
+}
