10e0afdb6fbf3fd0ac45433c00ffc417d3836ffe
himanshu dubey <hd156681@gmail.com>
2020-03-02 22:58:48 +0530

    line 4 changed

--- pointer.cpp
+++ pointer.cpp
@@ -1,12 +1,12 @@
 #include<iostream>
 using namespace std;
-int main()
+int main()//main added
 {
 	inta,b=20,c=50;
 	int *p,*q,*r; //pointer declaration
-	p=&a;
+	p=&a;//p defined
 	q=&b;
 	r=&c;
 	*p=*q+*r;
-	cout<<"Add"<<*p; //result
+	cout<<"Add"<<*p; //result out
 }
