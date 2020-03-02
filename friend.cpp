3105ae949caa501537c966203404882cbc731760
himanshu dubey <hd156681@gmail.com>
2020-03-02 22:53:14 +0530

    line 9 changed

--- friend.cpp
+++ friend.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-class base
+class base//base defined
 {
 public:
 int a;
@@ -10,11 +10,11 @@ base (int i,int j)
 a=i;
 b=j;
 }
-friend class derived;
+friend class derived;//friend function defined
 };
 
 
-class derived
+class derived//class defined
 {
 public:
 int min(base x);
@@ -26,11 +26,11 @@ return x.a < x.b ? x.a : x.b;
 
 }
 
-int main()
+int main()//main added
 {
 base obj(10,20);
 
 derived d;
 cout<<d.min(obj)<<endl;
 return 0;
-}
+}//end of programme
