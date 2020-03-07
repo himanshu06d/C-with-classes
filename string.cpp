c30a649eb0b2c60cc5ae063bb7c508f271d1ecf6
himanshu dubey <hd156681@gmail.com>
2020-03-07 22:40:29 +0530

    string

--- string.cpp
+++ string.cpp
@@ -7,7 +7,7 @@ int main()
 		u[200]="easy@gmail.com",p[200]="ani@789";
 	char u1[200],p1[200];
 	cout<<"Enter username\n";
-	cin>>u1;
+	cin>>u1;//insertion
 	cout<<"Enter password\n";
 	cin>>p1;
 	if(strcmp(u,u1)==0 && strcmp(p,p1)==0)//if true then this --execute
