0e454e83d2ff6405888de61fbb43a240741053a7
himanshu dubey <hd156681@gmail.com>
2020-03-16 16:23:22 +0530

    enque deque 1d array

---
+++ enque_deque.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+class queue
+{
+private:
+    int rear,fron,capacity;
+    int *arr,choice;
+public:
+    queue(int y):capacity(y)
+    {
+        rear=fron=-1;
+        arr=new int[capacity];
+    }
+    int isemp()
+    {
+        return(fron==-1);
+    }
+    int isfull()
+    {
+        return(rear==capacity-1);
+    }
+    void enqueue();
+    void dequeue();
+    void menu();
+};
+void queue:: enqueue()
+    {
+        if(isfull())
+        {
+            cout<<"Queue is full:"<<endl;
+        }
+        else
+        {
+            rear++;
+            cout<<"Enter data:"<<endl;
+            cin>>arr[rear];
+            if(fron==-1)
+            {
+                fron=rear;
+            }
+        }
+    }
+void queue:: dequeue()
+    {
+        if(isemp())
+        {
+            cout<<"Queue is empty"<<endl;
+        }
+        else
+        {
+            cout<<"Data is: "<<arr[fron]<<endl;
+            fron++;
+            if(rear+1==fron)
+            {
+                rear=fron=-1;
+            }
+        }
+    }
+void queue:: menu()
+{
+        cout<<"Enter 1 for adding element in queue:"<<endl;
+        cout<<"Enter 2 for deleting element from queue:"<<endl;
+        while(1)
+        {
+            cout<<"Enter your choice:"<<endl;
+            cin>>choice;
+            switch(choice)
+            {
+            case 1:
+                enqueue();
+                break;
+            case 2:
+                dequeue();
+                break;
+            }
+        }
+    }
+int main()
+{
+    int capacity; //variable to store capacity
+    cout<<"Enter capacity of queue:"<<endl;
+    cin>>capacity;
+    queue q(capacity);
+    q.menu();
+}
+
+
