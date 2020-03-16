272a51a6d4f0b822dab23c9926788a03c67af117
himanshu dubey <hd156681@gmail.com>
2020-03-16 16:11:59 +0530

    decimal to binary

---
+++ deci.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int x[10], n, i;
+cout<<"Enter the number: ";
+cin>>n;
+for(i=0; n>0; i++)
+{
+x[i]=n%2;
+n= n/2;
+}
+cout<<"Binary of the given number= "<<endl;
+for(i=i-1 ;i>=0 ;i--)
+{
+cout<<x[i];
+}
+
+
