48757f60115e7cd1215e2d8b7b4265377bbb1d4c
himanshu dubey <hd156681@gmail.com>
2020-04-19 03:07:35 +0530

    addtion via function

---
+++ fun2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int add(int x,int y)
+{
+	int z;
+	z=x+y;
+	retyrn z;
+}
+int main (int a, int b)
+{
+	int c;
+	c=add(10,20);
+	cout<<"addition of two numbres is : \n "<<c<<endl;
+return 0;
+}
+
