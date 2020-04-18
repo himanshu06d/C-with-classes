e2b0a007be5498ec1d320f3a9ac1b9f0b2947cba
himanshu dubey <hd156681@gmail.com>
2020-04-19 03:23:36 +0530

    class example

---
+++ class.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class university
+{
+	public:
+		char upes;
+	private:
+		cout<<"My university name is : \n "<<char<<endl;
+};
+class year
+{
+	public:
+		int a=1;
+	private:
+		cout<<"I studied in year : \n "<<a<<endl;
+};
+class subject
+{
+	public:
+		char devops;
+	private:
+		cout<<"My subject is : /n"<<char<<endl;
+};
+int main()
+{
+	university 1.university;
+	year 2.year;
+	subject 3.subject;
+	return 0;
+}
